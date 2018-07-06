df9668857596c53cca3d3c6e86ed7bfb7d9f6248
José Miguel Vega <jvegamoreno@outlook.com>
2018-07-06 16:50:09 +0200

    Added test cases and new index
    for user_permission and create_user_permissions migration

--- server/models/user_permission_test.go
+++ server/models/user_permission_test.go
@@ -47,3 +47,57 @@ func Test_UserPermission(t *testing.T) {
 	}
 
 }
+
+func Test_UserPermission_UniqueEntry(t *testing.T) {
+
+	var u, u2 = &User{}, &User{}
+	var p = &Permission{}
+
+	u.Username = "pepep-testus"
+	p.Name = "permiso-pep"
+	p.Description = "permiso de pepep"
+
+	u2.Username = "pepo-testus"
+
+	if _, err := db.ValidateAndCreate(u); err != nil {
+		t.Error(err)
+	}
+
+	if _, err := db.ValidateAndCreate(u2); err != nil {
+		t.Error(err)
+	}
+
+	if _, err := db.ValidateAndCreate(p); err != nil {
+		t.Error(err)
+	}
+
+	var up, dup, up2 = &UserPermission{}, &UserPermission{}, &UserPermission{}
+
+	up.UserID = u.ID
+	up.PermissionID = p.ID
+
+	dup.UserID = u.ID
+	dup.PermissionID = p.ID
+
+	up2.UserID = u2.ID
+	up2.PermissionID = p.ID
+
+	if errs, err := db.ValidateAndCreate(up); err != nil || errs.HasAny() {
+		t.Error(errs)
+		t.Error(err)
+	}
+
+	if errs, err := db.ValidateAndCreate(dup); err == nil && !errs.HasAny() {
+		t.Error("missed expected duplication error")
+	}
+
+	if errs, err := db.ValidateAndCreate(up2); err != nil || errs.HasAny() {
+		t.Error(errs)
+		t.Error(err)
+	}
+}
+
+//TODO
+func Test_UserPermission_RemovePerms(t *testing.T) {
+	t.Fatal("unimplemented")
+}
